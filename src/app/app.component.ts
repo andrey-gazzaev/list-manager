9f4a903a95826cb573e05773cf11b6e13528cc55
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2024-12-18 11:00:05 +0700

    Change app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { ListManager } from './list-manager/list-manager';
+
+type User = {
+  readonly createdAt: string;
+  readonly name: string;
+  readonly avatar: string;
+  readonly id: number;
+};
+
+type filter = {
+  readonly search: string;
+};
+
+type Sort = {
+  readonly direction: 'asc' | 'des';
+  readonly field: keyof User;
+};
+
+const DEFAULT_SORT: Sort = {
+  direction: 'asc',
+  field: 'name',
+};
+
+const DEFAULT_FILTER: filter = {
+  search: '',
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'list-manager';
+
+  private readonly router = inject(Router);
+
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  private readonly httpClient = inject(HttpClient);
+
+  protected readonly listManager = new ListManager<User[], Sort, filter>({
+    initFilter: DEFAULT_FILTER,
+    initSort: DEFAULT_SORT,
+    fetch: ({ sort, filter, pagination }) => {
+      console.log(sort, filter, pagination);
+      return this.httpClient.get<User[]>(
+        'https://6641a41b3d66a67b3434756c.mockapi.io/api/v1/users'
+      );
+    },
+    activatedRoute: this.activatedRoute,
+    router: this.router,
+  });
+
+  protected onSearchChange(event: Event): void {
+    this.listManager.setFilter({
+      ...this.listManager.queryParams().filter,
+      search: (event.target as HTMLInputElement).value,
+    });
+  }
 }
